925c1e765a3854ae04929dada856b1fc16c64f5a
TechDevAlex <128148527+TechDevAlex@users.noreply.github.com>
2024-07-30 16:45:20 +0200

    test 002

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -5,15 +5,17 @@
 
 /* global Office */
 
+document.getElementById("placeholder_feature_button").onclick = () => {
+  console.log("placeholder_feature_button clicked");
+  window.location.href = "https://techdevalex.github.io/Excel_Addin_Hosting/src/taskpane/feature_placeholder/feature_placeholder.html";
+
+}
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     // document.getElementById("run").onclick = run;
-    document.getElementById("placeholder_feature_button").onclick = () => {
-      console.log("placeholder_feature_button clicked");
-      window.location.href = "https://techdevalex.github.io/Excel_Addin_Hosting/src/taskpane/feature_placeholder/feature_placeholder.html";
-
-    }
+  
   }
 });
